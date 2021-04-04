731b9d3f21ac8ebc790a0c55bedab5f215b3a837
Aditya Patil <73546596+AdityaCyberSafe@users.noreply.github.com>
2021-04-04 12:27:19 +0000

    Update SES.py

--- SES.py
+++ SES.py
@@ -2,63 +2,277 @@
 ## © 2021 Aditya Patil.
 ## The Above Copyright Notice Should Remain in all versions of this file.
 
-import pandas as pd
+"""
+------------------------------------------------------------------------------------
+Midterm Project: SES Cipher
 
-encryptionkey = pd.read_csv(r"decodekeynew.csv",
-                            sep=',', names=['Character', 'Byte'], header=None, skiprows=[0])
+Objective: Create a program that creates a SES matrix
+	using the given key and either encrypts or decrypts a
+	message using the generates matrix.
 
-df = pd.DataFrame(data=encryptionkey)
+Author: Aditya Patil
 
-df['Character'] = df['Character'].astype(str)
-df['Byte'] = df['Byte'].astype(str)
+Date: April 4, 2021
+------------------------------------------------------------------------------------
+Examples: (newlines must be removed)
+------------------------------------------------------------------------------------
+Key: MONARCHY
+Ciphertext: UGRMKCSXHMUFMKBTOXGCMVATLUIV
+Resulting Plaintext: WEAREDISCOVEREDSAVEYOURSELFX
+------------------------------------------------------------------------------------
+Key: UNITEDSTATESOFAMERICA
+Plaintext:
+CONGRESS SHALL MAKE NO LAW RESPECTING AN ESTABLISHMENT OF RELIGION OR PROHIBITING 
+THE FREE EXERCISE THEREOF OR ABRIDGING THE FREEDOM OF SPEECH OR OF THE PRESS OR 
+THE RIGHT OF THE PEOPLE PEACEABLY TO ASSEMBLE AND TO PETITION THE GOVERNMENT FOR 
+A REDRESS OF GRIEVANCES
 
-def split(message):
-    return [char for char in message]
+Resulting Ciphertext: 
+BAERG NAWAW DKCXH CSLUI APSXG NOKIG ETERS INFIO CPNAV HUIOB SGIQE CTATS BKBSL UCTET
+ERUPF GGNIZ IZNGL AFNUP NGTFD FCSGC UACEE RUPFG GNUFD BFDOK IZIGP DBSOE QUKBN FAFBN
+QUCNM QOBOE QUQTB YQIQT CLIFC PTOFO AWFNR GQISI OUBYU ETETA IEQUB FZUKS GUIED FCSGN 
+SMNFA FGQCN UZSIG IAW
+------------------------------------------------------------------------------------
+"""
 
-message = 'Type the message that you want to be encrypted in this variable.'
+alpha = ['A','B','C','D','E','F','G','H','I','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 
-message_split = split(message)
+"""
+Function: key_key()
+Brief: Receives the key as input from the user. Handles valid characters and 
+	special cases.
+Parameter: None
+Return: Returns the key entered by the user.
+"""
+def get_key():
+	k = raw_input().upper()
+	key = []
+	for char in k:
+		if char in alpha and char not in key: # add the character to the matrix if it's valid and not already in the matrix
+			key.append(char)
+		elif char is "J": # handle the case when the letter J appears in the key
+			key.append("I")
+	for char in alpha:
+		if char not in key: # add the rest of the alphahet not appearing in the key to the matrix
+			key.append(char)
+	return key
 
-print(message_split)
 
-def code_message():
-    coded_message = ""
+"""
+Function: gen_matrix(key)
+Brief: Generates a SES matrix with the given key.
+Parameter: key - The key to use for generating a SES matrix.
+Return: Returns the keyed SES matrix.
+"""
+def gen_matrix(key):
+	matrix = []
+	counter = 0
+	if key == '': # create a blank matrix
+		for xcounter in xrange(5):
+			x = []
+			for ycounter in xrange(5):
+				x.append(alpha[counter])
+				counter += 1
+			matrix.append(x)
+	else: # create a keyed matrix
+		for xcounter in xrange(5):
+			x = []
+			for ycounter in xrange(5):
+				x.append(key[counter])
+				counter += 1
+			matrix.append(x)
+	return matrix
 
-    for i in range(len(message_split)):
-        j = message_split[i]
-        try:
-            coded_char = encryptionkey.loc[encryptionkey['Character'] == j, 'Byte'].iloc[0]
-            print(type(coded_char))
 
-        # To handle if character is not in our decryption list
-        except:
-            print('Unrecognized Character')
-            coded_char = '@@@'
+"""
+Function: print_matrix(matrix)
+Brief: Prints the given SES matrix.
+Parameter: matrix - The SES matrix to print out.
+Returns: None.
+"""
+def print_matrix(matrix):
+	for counter in xrange(5):
+		print "%c %c %c %c %c" % (matrix[counter][0], matrix[counter][1], matrix[counter][2], matrix[counter][3], matrix[counter][4])
+	print "\n"
 
-        coded_message = coded_message + coded_char
-    return coded_message
 
-coded_message = code_message()
+"""
+Fucntion: get_message()
+Brief: Receives a message an input from the user. Handles valid characters and 
+	special cases.
+Parameter: None
+Return: Returns the resulting message.
+"""
+def get_message():
+	m = raw_input()
+	m2 = []
+	for char in m.upper():
+		if char in alpha: # handle valid characters in the message
+			m2.append(char)
+		elif char == "J": # handle the case when "J" appears in the message
+			m2.append("I") 
+		elif char == ".": # swap out the period with an x, for convenience
+			m2.append("X")
+	return ''.join(m2)
 
-print('Your coded message:', code_message(), '\n')
 
-def decode_message(message):
-    new_word = ''
-    decoded_message = []
+"""
+Function encrypt(message, key_matrix)
+Brief: Performs encryption of the given message with the keyed SES
+	matrix.
+Parameter: message - The message to perform encryption on.
+Parameter: key_matrix - The keyed SES matrix to use for encryption.
+Return: Returns nothing, the resulting ciphertext is printed at the end
+	of the function.
+"""
+def encrypt(message, key_matrix):
+	coords = []
+	ciphertext = []
+	digraphs = parse_message(message)
 
-    for i in range(0, len(message), 2):
-        j = message[i:i + 2]
-        index_nb = df[df.eq(j).any(1)]
+	for d in digraphs:
+		swap = []
+		temp = []
+		coords = get_coords(d, key_matrix)
+		if coords[0][0] == coords[1][0]: # digraph lies on same x axis
+			x,y  = ((coords[0][0], (coords[0][1] + 1) % 5))
+			swap.append((x,y))
+			x,y  = ((coords[1][0], (coords[1][1] + 1) % 5))
+			swap.append((x,y))
+		elif coords[0][1] == coords[1][1]: # digraph lies on same y axis
+			x,y  = (((coords[0][0] + 1) % 5), coords[0][1])
+			swap.append((x,y))
+			x,y  = (((coords[1][0] + 1) % 5), coords[1][1])
+			swap.append((x,y))
+		else: # digraph lies on different x & y axis
+			swap.append((coords[0][0], coords[1][1]))
+			swap.append((coords[1][0], coords[0][1]))
 
-        df2 = index_nb['Character'].tolist()
+		for x,y in swap:
+			ciphertext.append(key_matrix[x][y])
 
-        s = [str(x) for x in df2]
+	print "Your encrypted message is: %s " % ''.join(ciphertext)
 
-        decoded_message = decoded_message + s
 
-    new_word = ''.join(decoded_message)
+"""
+Function decrypt(message, key_matrix)
+Brief: Performs decryption of the given message with the keyed SES
+	matrix.
+Parameter: message - The message to perform decryption on.
+Parameter: key_matrix - The keyed SES matrix to use for decryption.
+Return: Returns nothing, the resulting plaintext is printed at the end
+	of the function.
+"""
+def decrypt(message, key_matrix):
+	coords = []
+	plaintext = []
+	digraphs = parse_message(message)
 
-    return new_word
+	for d in digraphs:
+		swap = []
+		temp = []
+		coords = get_coords(d, key_matrix)
+		if coords[0][0] == coords[1][0]: # digraph lies on same x axis
+			x,y  = ((coords[0][0], (coords[0][1] - 1) % 5))
+			swap.append((x,y))
+			x,y  = ((coords[1][0], (coords[1][1] - 1) % 5))
+			swap.append((x,y))
+		elif coords[0][1] == coords[1][1]: # digraph lies on same y axis
+			x,y  = (((coords[0][0] - 1) % 5), coords[0][1])
+			swap.append((x,y))
+			x,y  = (((coords[1][0] - 1) % 5), coords[1][1])
+			swap.append((x,y))
+		else: # digraph lies on different x & y axis
+			swap.append((coords[0][0], coords[1][1]))
+			swap.append((coords[1][0], coords[0][1]))
 
-coded_message_str = str(coded_message)
-print('Your decoded message:', decode_message(coded_message_str))
+		for x,y in swap:
+				plaintext.append(key_matrix[x][y])
+
+	print "Your decrypted message is: %s " % ''.join(plaintext)
+
+
+"""
+Function: parse_message(message)
+Brief: Parses the message provided by the user. Prepares the text by handling
+	cases where double letters appear next to each other. Ignores non-alphabetic
+	characters, numbers, and punctuation.
+Parameter: message - The message entered by the user.
+Return: Returns an array of digraphs resulting from the given message.
+"""
+def parse_message(message):
+	digraphs = []
+	while len(message) > 0:
+		digraph = message[:2]
+		if len(digraph) == 1: # trailing single chracter at the end of the message
+			digraph = digraph = "%c%c" % (digraph[0], "X")
+			digraphs.append(digraph)
+			message = message[1:]
+		elif digraph[0] == digraph[1]: # handle double letters appearing in the same digraph
+			digraph = "%c%c" % (digraph[0], "X")
+			digraphs.append(digraph)
+			message = message[1:]
+		else: # add the digraph to the list
+			digraphs.append(digraph)
+			message = message[2:]
+
+	return digraphs 
+
+
+"""
+Function: get_coords(digraph, key_matrix)
+Brief: Returns the coordinates of the letters in the given digraph from the provided keyed matrix. 
+Parameter: digraph - The two-letter digraph to lookup in the key matrix.
+Parameter: key_matrix - The keyed SES matrix to perform the lookup on.
+Return: Returns an array with the coordinates of the given digraph.
+"""
+def get_coords(digraph, key_matrix):
+	coords = []
+	for char in digraph:
+		for x in xrange(5):
+			for y in xrange(5):
+				if key_matrix[x][y] == char:
+					coords.append((x,y))
+	return coords
+					
+
+def main():
+	m = gen_matrix('')
+	print "\nInitial SES matrix:\n"
+	print_matrix(m)
+
+	print "Enter a key:"
+	k = get_key()
+
+	print "\nKeyed SES matrix:\n"
+	m = gen_matrix(k)
+	print_matrix(m)
+
+	decision = ""
+	while decision is not "1" and decision is not "2":
+		print "Would you like to encrypt or decrypt a message?"
+		print "1 - Encrypt message"
+		print "2 - Decrypt Message"
+		print "\nDecision:"
+		decision = raw_input()
+	
+	if decision == "1":
+		print "\nEncrypt Message:"
+		print "Enter the message you would like to encrypt. \nThe only valid characters are the letters A-Z. \nPeriods may be denoted with an X"
+		message = get_message()
+		print "The message you entered was: %s" % message
+		ciphertext = encrypt(message, m)
+
+	elif decision == "2":
+		print "\nDecrypt Message:"
+		print "Enter the message you would like to decrypt. \nThe only valid characters are the letters A-Z."
+		message = get_message()
+		print "The message you entered was: %s" % message
+		plaintext = decrypt(message, m)
+
+	else:
+		print "Invalid Entry"
+
+
+if __name__ == "__main__":
+	main()
